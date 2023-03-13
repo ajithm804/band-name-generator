be3d159160a718feb69dac24a29f7a9dcabf798c
Ajith Mathew <ajithm804@gmail.com>
2023-03-13 22:36:17 +0530

    Add files via upload

---
+++ Band Name Genrator.py
@@ -0,0 +1,4 @@
+print("Welcome to the Band Generator")
+city=input("What is your name of the city you grew you up in?\n")
+pet=input("What is your pet name?")
+print("Your band name could be " + city + pet)
